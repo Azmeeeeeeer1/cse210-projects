ba55e0417ce43e9bfb46c2ea95d9bd9eb49467e5
Azmeer Ahmad <azmeeeeeeer@gmail.com>
2026-07-04 21:24:02 +0300

    finished week 1 exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your percentage? ");
+        string userInput = Console.ReadLine();
+        int percentage = int.Parse(userInput);
+
+        string letter;
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"You got {letter}");
+
+        if (letter == "D" || letter == "F")
+        {
+            Console.WriteLine("You will get it next time if you study hard enough!!");
+        }
+        else
+        {
+            Console.WriteLine("Congratulations you passed!!!");
+        }
     }
 }
